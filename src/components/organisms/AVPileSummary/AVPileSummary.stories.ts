2969846012c6c0ca529a220c496dfc83c2439a88
Martin Laursen <martin@assemblyvoting.com>
2025-09-23 12:06:58 +0200

    revert this here too

--- src/components/organisms/AVPileSummary/AVPileSummary.stories.ts
+++ src/components/organisms/AVPileSummary/AVPileSummary.stories.ts
@@ -196,7 +196,7 @@ export const SummaryWithPrefilled = {
     isEditing: false,
     maximumOptionsShown: 3,
     pileIndex: 0,
-    totalPiles: 1,
+    totalPiles: 3,
     selectionPile: getSelectionPile(["single", "weighted"]),
     isPrefilled: true,
   },
