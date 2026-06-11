72b4ca1850da82369b2242fee091bd02b4ba2ca5
Alexis Toledo <112619860+av-alexistoledo@users.noreply.github.com>
2026-06-11 11:08:37 +0200

    Another one

--- src/components/atoms/AVOptionCheckbox/AVOptionCheckbox.vue
+++ src/components/atoms/AVOptionCheckbox/AVOptionCheckbox.vue
@@ -49,12 +49,15 @@ const onToggled = (): void => emits("toggled");
 const accessibilityAttributes = computed(() => {
   if (!props.optionReference) return {};
 
-  return {
+  const attrs: Record<string, string | number> = {
     id: `option_${props.optionReference}_checkbox_${props.checkBoxIndex}`,
     "aria-labelledby": `option_${props.optionReference}_title option_${props.optionReference}_tick option_${props.optionReference}_subtitle`,
     "aria-describedby": `option_${props.optionReference}_description option_${props.optionReference}_handle option_${props.optionReference}_partial_results`,
-    tabindex: props.disabled ? undefined : 0,
   };
+
+  if (props.disabled) attrs.tabindex = 0;
+
+  return attrs;
 });
 
 /**
