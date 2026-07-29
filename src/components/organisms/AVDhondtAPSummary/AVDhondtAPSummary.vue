2fc87faaec3d22363e6d44ee0ad59540e0cd5f8d
Alexis Toledo <112619860+av-alexistoledo@users.noreply.github.com>
2026-07-29 13:28:16 +0200

    Fix typos

--- src/components/organisms/AVDhondtAPSummary/AVDhondtAPSummary.vue
+++ src/components/organisms/AVDhondtAPSummary/AVDhondtAPSummary.vue
@@ -25,7 +25,7 @@ const props = defineProps({
     required: true,
   },
   seats: {
-    typ: Number,
+    type: Number,
     required: true,
   },
   hideElected: {
--- src/components/organisms/AVDhondtSummary/AVDhondtSummary.vue
+++ src/components/organisms/AVDhondtSummary/AVDhondtSummary.vue
@@ -24,7 +24,7 @@ const props = defineProps({
     required: true,
   },
   seats: {
-    typ: Number,
+    type: Number,
     required: true,
   },
   locale: {
